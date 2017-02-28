1270af473fad9ad215853b1385df955287efbd4f
Naomi Villanueva <naovillaj@gmail.com>
2017-02-28 15:10:19 -0500

    calculando IMC

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,13 @@
-alert ("hola");
-/*function imc(){
-	var peso = prompt("Ingrese peso en Kg: ");
-	var talla
-}*/
+
+var imprimir = document.getElementById("imprimir");
+var records= document.getElementById('records');
+
+imprimir.onclick = function (){
+
+	var peso = document.getElementById("peso").value;
+	var talla = document.getElementById("talla").value;
+
+	var cimc = peso/Math.pow(talla,2);
+	alert ("Tu IMC es: "+ cimc.toFixed(2));
+}
+
