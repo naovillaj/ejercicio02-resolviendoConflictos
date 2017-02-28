018f173c69105761995490fad3bc0337d6ef8334
Flor Tello <flor.26071994@gmail.com>
2017-02-28 15:12:34 -0500

    Agreado function calcula IMC

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
-alert ("hola");
-/*function imc(){
-	var peso = prompt("Ingrese peso en Kg: ");
-	var talla
-}*/
+function imc(){
+	var peso = (document.getElementById('peso').value);
+	var talla = (document.getElementById('estatura').value);
+	if(peso !== "" || talla !== ""){
+		alert("El indice de tu masa corporal es : " + (peso / Math.pow(talla,2)).toFixed(2));
+	}
+	else{
+		alert("Error de Datos");
+	}
+}
